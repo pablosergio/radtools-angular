3fca393f767ef5fa32bb50584f59b46704ee8d39
Administrator@solutions.com <Administrator@solutions.com>
2017-08-30 18:00:46 -0400

    css

--- src/app/settings-application/settings-application.module.ts
+++ src/app/settings-application/settings-application.module.ts
@@ -1,10 +1,10 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsApplicationRoutingModule } from './settings-application-routing.module';
 import { SettingsApplicationComponent } from './settings-application.component';
+import { SettingsApplicationService } from './settings-application.service';
 import { GridApplicationComponent } from './grid-application/grid-application.component';
 import { PanelModule, DataTableModule, DropdownModule, SplitButtonModule } from 'primeng/primeng';
-import { SettingsApplicationService } from './settings-application.service';
 
 
 @NgModule({
@@ -14,8 +14,7 @@ import { SettingsApplicationService } from './settings-application.service';
     PanelModule,
     DataTableModule,
     DropdownModule,
-    SplitButtonModule,
-    SettingsApplicationRoutingModule
+    SplitButtonModule
   ],
   declarations: [
     SettingsApplicationComponent,
